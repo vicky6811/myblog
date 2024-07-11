823cdce1ebcec793e56a9595645570adff28bf30
vicky6811 <your_email@example.com>
2024-07-11 03:57:40 -0400

    update

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,23 +1,25 @@
 const Home = () => {
 
     const handleClick  = (e) => {
-        console.log('hello, ninjas', e);
+        //console.log('hello, ninjas', e);
     }
 
-    const handleClickAgain = (name, e) => {
-        console.log('hello ' + name, e.target);
-    }
+    //const handleClickAgain = (name, e) => {
+       // console.log('hello ' + name, e.target);
+    //}
 
     return (
         <div className="home">
             <h2>Homepage</h2>
             <button onClick= {handleClick} >Click me</button>
-            {/*<button onClick={handleClickAgain('mario')}>Click me again</button>*/}
-            <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
 
+            {/*<button onClick={handleClickAgain('mario')}>Click me again</button>*/}
             
+
+    
+            {/*<button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>*/}
             {/*
-            //another way to write above
+            //another way to write above without the addtional curly bracket
                 <button onClick={() => {
                     //console.log('hello')
                     handleClickAgain('mario')
