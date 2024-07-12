669b67890ba4d918a54408cbe42abbd6ffc5ab31
vicky6811 <your_email@example.com>
2024-07-12 00:38:05 -0400

    finish

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   //const title = 'Welcome to the new blog';
@@ -24,6 +25,9 @@ function App() {
           </Route>
           <Route path="/blogs/:id"> 
             <BlogDetails />
+          </Route>
+          <Route path = "*">
+            <NotFound />
           </Route>
         </Switch>
 
